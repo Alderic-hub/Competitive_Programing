eb6f9a4e7cc86b33acbf050b143170e0752261be
Alderic-hub <aldricrealm@gmail.com>
2026-03-10 18:48:52 +0300

    Create Ehab Is an Odd Person.py

---
+++ CodeForces/Ehab Is an Odd Person.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+ 
+odd = False
+even = False 
+for num in arr:
+    if num % 2:
+        odd = True
+    else:
+        even = True 
+if odd and even:
+    arr.sort()
+ print(*arr)
